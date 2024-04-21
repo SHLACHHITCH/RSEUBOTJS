f909127fcd1b617b84fdc3518dd590b0c5434969
SHLACHHITCH <56740690+SHLACHHITCH@users.noreply.github.com>
2024-04-22 02:57:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000; // Устанавливаем порт 10000 по умолчанию или получаем его из переменной окружения PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
+// Прослушиваем порт на хосте 0.0.0.0
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
